78264f3a9acfe0b6f1f94ad3e2a6b2d10ec51121
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-11-18 11:42:08 -0100

    new commit

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,14 @@ import PasswordLink from "@/components/PasswordLink.vue";
 import ResetPassword from "@/components/ResetPassword.vue";
 import PasswordUpdate from "@/components/PasswordUpdate.vue";
 import RegisterPage from "@/components/RegisterPage.vue";
-// import AdminRegister from "@/components/AdminRegister.vue";
-// import AdminLogin from "@/components/AdminLogin.vue";
-// import AdminDashboard from "@/components/AdminDashboard.vue";
-// import AdminResetPassword from "@/components/AdminResetPassword.vue";
-// import AdminPasswordLink from "@/components/AdminPasswordLink.vue";
-// import AdminPasswordUpdate from "@/components/AdminPasswordUpdate.vue";
+import AdminRegister from "@/components/AdminRegister.vue";
+import AdminLogin from "@/components/AdminLogin.vue";
+import AdminDashboard from "@/components/AdminDashboard.vue";
+import AdminResetPassword from "@/components/AdminResetPassword.vue";
+import AdminPasswordLink from "@/components/AdminPasswordLink.vue";
+import AdminPasswordUpdate from "@/components/AdminPasswordUpdate.vue";
 import UserNews from "@/components/UserNews.vue";
-// import AdminNews from "@/components/AdminNews.vue";
+import AdminNews from "@/components/AdminNews.vue";
 
 
 
@@ -31,13 +31,13 @@ const routes = [
 
 
 
-    // {path: "/admin/register", component: AdminRegister},
-    // {path: "/admin/login", component: AdminLogin},
-    // {path: "/admin/dashboard", component: AdminDashboard},
-    // {path: "/admin/passwordupdate", component: AdminPasswordUpdate},
-    // {path: "/admin/reset-password/:token", component: AdminResetPassword},
-    // {path: "/admin/passwordlink", component: AdminPasswordLink},
-    // {path: "/admin/News", component: AdminNews},
+    {path: "/admin/register", component: AdminRegister},
+    {path: "/admin/login", component: AdminLogin},
+    {path: "/admin/dashboard", component: AdminDashboard},
+    {path: "/admin/passwordupdate", component: AdminPasswordUpdate},
+    {path: "/admin/reset-password/:token", component: AdminResetPassword},
+    {path: "/admin/passwordlink", component: AdminPasswordLink},
+    {path: "/admin/News", component: AdminNews},
 
 
 
